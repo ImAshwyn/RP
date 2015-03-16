917e45fc944740715a6c319241596c283ecde4c4
Ashwin Kini <ashwin.kini@icloud.com>
2015-03-16 15:34:17 +0530

    Initial Commit

---
+++ SocioStorage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocioStorage
+//
+//  Created by Ashwin Kini  on 16/03/15.
+//  Copyright (c) 2015 RookieParadise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
